f36e44dc743e6f6e1716c673b7003325e0bcf2bc
Jamie Arkin <jarkin@yext.com>
2013-07-12 12:07:38 -0400

    console log

--- js/storepages.js
+++ js/storepages.js
@@ -137,7 +137,8 @@
     ResultsPage.prototype.initializeMap = function() {
       var map,
           self = this;
-      
+      console.log(this.markers[1]);
+      console.log(markers[1]);
       var myLatlng = new google.maps.LatLng(this.markers[1], this.markers[1]);
 
       function initialize() {
