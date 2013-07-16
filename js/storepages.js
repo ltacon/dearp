fb2ca71dba7ba7580f9cc684f47cf7139e0bcf02
Chris Calabrese <cbreezy@yext.com>
2013-07-16 00:19:39 -0400

    Plebbery

--- js/storepages.js
+++ js/storepages.js
@@ -67,7 +67,7 @@
         $(".search-link").attr("href", "search/?query=" + location);   
 
         var zipcode = window.location.href.substring(window.location.href.lastIndexOf('=') + 1);
-        $(".search-zip-code").val(zipcode);
+        $(".search-zip-code").text(zipcode);
       }
    
       $("input.zip-city-state").change(displayVals);
